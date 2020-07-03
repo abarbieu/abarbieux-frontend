16f2ba9a868237934f143ac9730bc6793f94a8a9
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-07-03 16:17:31 -0700

    react set up

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Todos from './components/Todos';
+import Header from './components/style/Header';
+import uuid from 'uuid';
+
+import './App.css';
+
+class App extends React.Component {
+	state = {
+		todos : [
+			{
+				id        : uuid.v4(),
+				title     : 'sleep',
+				completed : false
+			},
+			{
+				id        : uuid.v4(),
+				title     : 'eat food',
+				completed : false
+			},
+			{
+				id        : uuid.v4(),
+				title     : 'ride',
+				completed : false
+			}
+		]
+	};
+	markComplete = (id) => {
+		this.setState({
+			todos : this.state.todos.map((todo) => {
+				if (todo.id === id) {
+					todo.completed = !todo.completed;
+				}
+				return todo;
+			})
+		});
+	};
+	render () {
+		return (
+			<div className="App">
+				<Header />
+				<Todos
+					todos={this.state.todos}
+					markComplete={this.markComplete}
+				/>
+			</div>
+		);
+	}
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>App</h1>
+//     </div>
+//   );
+// }
+
+export default App;
