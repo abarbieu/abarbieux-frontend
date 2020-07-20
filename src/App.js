82824550fe09f885ae379f75376281dbbb00854b
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-07-20 01:32:04 -0700

    reset merge

--- src/App.js
+++ src/App.js
@@ -1,31 +1,95 @@
 import React from 'react';
-import MenuItem from './components/selector/MenuItem';
-import MenuHeader from './components/selector/MenuHeader';
+import axios from 'axios';
+import Todos from './components/Todos';
+import Header from './components/style/Header';
+import AddTodo from './components/AddTodo';
+
 import './App.css';
 
 class App extends React.Component {
+  apiUrl = 'https://abarbieux.com/api/';
+
   state = {
-    btns : []
+    todos : []
   };
 
-  addBtn = (parent, title) => {
-    this.setState({
-      btns : [ ...this.state.btns, 'title' ]
+  getAllTodos = () => {
+    axios.get(this.apiUrl + 'todos').then((res) => {
+      this.setState({
+        todos : res.data
+      });
     });
+  };
+  componentDidMount () {
+    axios.get(this.apiUrl).then((res) => {
+      console.log(res.data);
+    });
+    this.getAllTodos();
+  }
+
+  markComplete = (id) => {
+    axios.put(this.apiUrl + `todos/${id}`).then((res) => {
+      console.log('toggled data: ' + res.data);
+    });
+    this.setState({
+      todos : this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  // Delete todo item
+  deleteTodo = (id) => {
+    axios.delete(this.apiUrl + `todos/${id}`).then((res) => {
+      console.log('deleted data: ' + res.data);
+    });
+    this.setState({
+      todos : [ ...this.state.todos.filter((todo) => todo.id !== id) ]
+    });
+  };
+
+  // Add todo item
+  addTodo = (title) => {
+    let newTodo = {};
+    axios
+      .post(this.apiUrl + 'todos/', {
+        title    : title,
+        complete : false
+      })
+      .then((res) => {
+        newTodo = res.data;
+        console.log(newTodo);
+
+        this.setState({ todos: [ ...this.state.todos, newTodo ] });
+      });
   };
 
   render () {
     return (
-      <div className='Tiled-back'>
-        <div className='container'>
-          <MenuHeader />
-          <div className='Menu-container'>
-            <MenuItem title='+' name='root' addItem={this.addBtn} />
-          </div>
+      <div className="App">
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            deleteTodo={this.deleteTodo}
+          />
         </div>
       </div>
     );
   }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>App</h1>
+//     </div>
+//   );
+// }
 
 export default App;
