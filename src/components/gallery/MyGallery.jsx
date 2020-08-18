94f897bed498556e857fdfc98b838304f7b1de25
Aidan Barbieux <abarbieu@calpoly.edu>
2020-08-18 02:53:39 -0700

    gallery stuff, navbar closing

--- src/components/gallery/MyGallery.jsx
+++ src/components/gallery/MyGallery.jsx
@@ -6,64 +6,82 @@ import Modal from 'react-bootstrap/Modal';
 // import ImgsViewer from 'react-images-viewer';
 
 export default function ImgGallery(props) {
-	// const [ thumbPos, setThumbPos ] = useState('bottom');
-	const [ currentImage, setCurrentImage ] = useState(0);
-	const [ viewerIsOpen, setViewerIsOpen ] = useState(false);
+  // const [ thumbPos, setThumbPos ] = useState('bottom');
+  const [ currentImage, setCurrentImage ] = useState(0);
+  const [ viewerIsOpen, setViewerIsOpen ] = useState(false);
+  const [ fullscreen, setFullscreen ] = useState(false);
 
-	const openLightbox = useCallback((event, { photo, index }) => {
-		setCurrentImage(index);
-		setViewerIsOpen(true);
-	}, []);
-	const closeLightbox = () => {
-		setCurrentImage(0);
-		setViewerIsOpen(false);
-	};
-	const lazyRender = (item) => {
-		return (
-			<div>
-				<LazyLoadImage
-					className="image-gallery-image"
-					effect="blur"
-					key={item.src}
-					alt={item.src}
-					srcSet={item.srcSet}
-					sizes={item.sizes}
-					placeholderSrc={item.placeholder || null}
-					src={item.src}
-				/>
-				{item.description && <span className="image-gallery-description">{item.description}</span>}
-			</div>
-		);
-	};
-	return (
-		<div style={{ width: '85vw' }} className="mx-auto p-0">
-			<GalleryGrid photos={props.photos} onPhotoClick={openLightbox} />
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setFullscreen(true);
+    setViewerIsOpen(true);
+  }, []);
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setFullscreen(false);
+    setViewerIsOpen(false);
+  };
+  // const lazyRender = (item) => {
+  //   return (
+  //     <div className="m-0 p-0" style={{ width: item.width, height: item.height }}>
+  //       <LazyLoadImage
+  //         className="image-gallery-image m-0 p-0"
+  //         effect="blur"
+  //         key={item.src}
+  //         alt={item.src}
+  //         srcSet={item.srcSet}
+  //         sizes={item.sizes}
+  //         placeholderSrc={item.placeholder || null}
+  //         src={item.src}
+  //       />
+  //       {item.description && <span className="image-gallery-description">{item.description}</span>}
+  //     </div>
+  //   );
+  // };
+  return (
+    <div style={{ width: '95vw' }} className="mx-auto p-0">
+      <GalleryGrid photos={props.photos} onPhotoClick={openLightbox} />
 
-			<Modal
-				show={viewerIsOpen}
-				onHide={closeLightbox}
-				dialogClassName="mx-auto modal-90w"
-				className="pic-modal"
-				centered
-			>
-				<Modal.Header className="pic-modal-header" closeButton>
-					<p style={{ color: 'white' }}>{props.photos[currentImage].src}</p>
-				</Modal.Header>
-				<Modal.Body>
-					{viewerIsOpen ? (
-						<ImageGallery
-							thumbnailPosition={props.thumbPos || 'bottom'}
-							startIndex={currentImage}
-							onSlide={setCurrentImage}
-							renderItem={lazyRender}
-							showIndex={true}
-							items={props.photos}
-						/>
-					) : null}
-				</Modal.Body>
-			</Modal>
-		</div>
-	);
+      <Modal
+        show={viewerIsOpen}
+        onHide={closeLightbox}
+        dialogClassName="mx-auto modal-90w"
+        className="pic-modal"
+        centered
+      >
+        <Modal.Header className="m-0 p-0 pic-modal-header" closeButton>
+          <p style={{ color: 'white' }}>{props.photos[currentImage].src}</p>
+        </Modal.Header>
+        <Modal.Body className="m-0 p-0">
+          {viewerIsOpen ? (
+            <ImageGallery
+              ref={(gallery) => {
+                if (gallery) {
+                  fullscreen ? gallery.fullScreen() : gallery.exitFullScreen();
+                }
+              }}
+              thumbnailPosition={props.thumbPos || 'bottom'}
+              startIndex={currentImage}
+              onSlide={setCurrentImage}
+              onScreenChange={(full) => {
+                setFullscreen(full);
+                // full && closeLightbox();
+              }}
+              // disableSwipe={true}
+              // isFullscreen={true}
+              lazyLoad={true}
+              useBrowserFullscreen={false}
+              // showBullets={true}
+              // renderItem={lazyRender}
+              showThumbnails={!fullscreen}
+              showIndex={true}
+              items={props.photos}
+            />
+          ) : null}
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
 }
 
 // import React, { useState, useCallback } from 'react';
--- src/components/navbar/MainNavbar.tsx
+++ src/components/navbar/MainNavbar.tsx
@@ -7,171 +7,138 @@ import Container from 'react-bootstrap/Container';
 import Tooltip from 'react-bootstrap/Tooltip';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
-function MainNavbar () {
-  const [ show, setShow ] = useState(false);
-  return (
-    <Navbar
-      style={{ backgroundColor: '#222233bb' }}
-      collapseOnSelect
-      fixed='top'
-      expand='md'
-      variant='dark'
-    >
-      <Container fluid='lg'>
-        <Navbar.Brand
-          style={{ color: '#60aaf0' }}
-          className='shr-btn'
-          href='#home'
-        >
-          <img alt='' src='/icons/sherbert.svg' width='32' height='32' />{' '}
-          {'Aidan Barbieux'}
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-        <Navbar.Collapse id='responsive-navbar-nav'>
-          <Nav className='mr-auto'>
-            <Nav.Link
-              className='nav-link shr-btn'
-              eventKey='home'
-              href='#/home/'
-            >
-              <img alt='Home' src='/icons/home.svg' width='32' height='32' />
-            </Nav.Link>
-            <Link
-              className='nav-link shr-btn'
-              to={{
-                pathname: '/explore/',
-                search: '?path=root',
-                state: { openPath: [ 'root' ] },
-              }}
-            >
-              <img
-                alt='Explore'
-                src='/icons/explore.svg'
-                width='32'
-                height='32'
-              />
-            </Link>
-            <Link
-              className='nav-link shr-btn'
-              to={{
-                pathname: '/art/ceramics',
-                search: '?path=art',
-                state: { openPath: [ 'root', 'art' ] },
-              }}
-            >
-              <img alt='Ceramics' src='/icons/art.svg' width='32' height='32' />
-            </Link>
-            <Link
-              className='nav-link shr-btn'
-              to={{
-                pathname: '/art/photos',
-                search: '?path=art',
-                state: { openPath: [ 'root', 'art' ] },
-              }}
-            >
-              <img
-                alt='Photos'
-                src='/icons/photos.svg'
-                width='32'
-                height='32'
-              />
-            </Link>
-            <Link
-              className='nav-link shr-btn'
-              to={{
-                pathname: '/explore/',
-                search: '?path=projects',
-                state: { openPath: [ 'root', 'projects' ] },
-              }}
-            >
-              <img
-                alt='Projects'
-                src='/icons/code.svg'
-                width='32'
-                height='32'
-              />
-            </Link>
-          </Nav>
-          <Nav className='ml-auto mr-3'>
-            <NavDropdown
-              className='my-auto '
-              title={
-                <div className=' ml-4 d-inline'>
-                  <img
-                    className='shr-btn'
-                    alt='Email'
-                    src='/icons/email.svg'
-                    width='32'
-                    height='32'
-                  />
-                </div>
-              }
-              id='collasible-nav-dropdown'
-            >
-              <Container className='justify-content-center m-0 p-0'>
-                <Navbar.Text
-                  style={{ fontSize: 12, color: 'black' }}
-                  className=' d-inline m-1 font-weight-bold'
-                >
-                  <OverlayTrigger
-                    placement='left'
-                    trigger='click'
-                    onToggle={() => {
-                      setShow(!show);
-                      setTimeout(() => {
-                        setShow(false);
-                      }, 800);
-                    }}
-                    show={show}
-                    overlay={<Tooltip id='copied'>Copied!</Tooltip>}
-                  >
-                    <span className='d-inline-block'>
-                      <button
-                        style={{ backgroundColor: 'transparent' }}
-                        className='d-inline border-0 mr-1 shr-btn'
-                        onClick={() => {
-                          navigator.clipboard.writeText('abarbieu@calpoly.edu');
-                        }}
-                      >
-                        <OverlayTrigger
-                          placement='top'
-                          delay={{ show: 550, hide: 400 }}
-                          overlay={
-                            <Tooltip id='copy-email'>Copy Email?</Tooltip>
-                          }
-                        >
-                          <img
-                            width='15'
-                            height='15'
-                            id='copyIcon'
-                            src='/icons/copy.svg'
-                            alt=''
-                          />
-                        </OverlayTrigger>
-                      </button>
-                    </span>
-                  </OverlayTrigger>
-                  abarbieu@calpoly.edu
-                </Navbar.Text>
-              </Container>
-            </NavDropdown>
-            <Nav.Link
-              className='shr-btn nav-link m-auto shr-btn'
-              eventKey='contact'
-              href='#/contact/'
-            >
-              <img
-                alt='Contact'
-                src='/icons/contact.svg'
-                width='32'
-                height='32'
-              />
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+function MainNavbar() {
+	const [ show, setShow ] = useState(false);
+	return (
+		<Navbar style={{ backgroundColor: '#222233bb' }} collapseOnSelect fixed="top" expand="md" variant="dark">
+			<Container fluid="lg">
+				<Navbar.Brand style={{ color: '#60aaf0' }} className="shr-btn" href="home">
+					<img alt="" src="/icons/sherbert.svg" width="32" height="32" /> {'Aidan Barbieux'}
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="mr-auto">
+						<Nav.Link className="nav-link shr-btn" eventKey="home" href="/home/">
+							<img alt="Home" src="/icons/home.svg" width="32" height="32" />
+						</Nav.Link>
+						<Nav.Link eventKey="explore">
+							<Link
+								className="nav-link shr-btn"
+								to={{
+									pathname: '/explore/',
+									search: '?path=root',
+									state: { openPath: [ 'root' ] }
+								}}
+							>
+								<img alt="Explore" src="/icons/explore.svg" width="32" height="32" />
+							</Link>
+						</Nav.Link>
+						<Nav.Link eventKey="ceramics">
+							<Link
+								className="nav-link shr-btn"
+								to={{
+									pathname: '/art/ceramics',
+									search: '?path=art',
+									state: { openPath: [ 'root', 'art' ] }
+								}}
+							>
+								<img alt="Ceramics" src="/icons/art.svg" width="32" height="32" />
+							</Link>
+						</Nav.Link>
+						<Nav.Link eventKey="photos">
+							<Link
+								className="nav-link shr-btn"
+								to={{
+									pathname: '/art/photos',
+									search: '?path=art',
+									state: { openPath: [ 'root', 'art' ] }
+								}}
+							>
+								<img alt="Photos" src="/icons/photos.svg" width="32" height="32" />
+							</Link>
+						</Nav.Link>
+						<Nav.Link eventKey="projects">
+							<Link
+								className="nav-link shr-btn"
+								to={{
+									pathname: '/explore/',
+									search: '?path=projects',
+									state: { openPath: [ 'root', 'projects' ] }
+								}}
+							>
+								<img alt="Projects" src="/icons/code.svg" width="32" height="32" />
+							</Link>
+						</Nav.Link>
+					</Nav>
+					<Nav className="mr-3">
+						<NavDropdown
+							className="my-auto"
+							title={
+								<div className=" ml-4 d-inline">
+									<img
+										className="shr-btn"
+										alt="Email"
+										src="/icons/email.svg"
+										width="32"
+										height="32"
+									/>
+								</div>
+							}
+							id="collasible-nav-dropdown"
+						>
+							<Container className="w-100 mx-auto p-0">
+								<Navbar.Text
+									style={{ fontSize: 12, color: 'black' }}
+									className=" d-inline m-1 font-weight-bold"
+								>
+									<OverlayTrigger
+										placement="left"
+										trigger="click"
+										onToggle={() => {
+											setShow(!show);
+											setTimeout(() => {
+												setShow(false);
+											}, 800);
+										}}
+										show={show}
+										overlay={<Tooltip id="copied">Copied!</Tooltip>}
+									>
+										<span className="d-inline-block">
+											<button
+												style={{ backgroundColor: 'transparent' }}
+												className="d-inline border-0 mr-1 shr-btn"
+												onClick={() => {
+													navigator.clipboard.writeText('abarbieu@calpoly.edu');
+												}}
+											>
+												<OverlayTrigger
+													placement="top"
+													delay={{ show: 550, hide: 400 }}
+													overlay={<Tooltip id="copy-email">Copy Email?</Tooltip>}
+												>
+													<img
+														width="15"
+														height="15"
+														id="copyIcon"
+														src="/icons/copy.svg"
+														alt=""
+													/>
+												</OverlayTrigger>
+											</button>
+										</span>
+									</OverlayTrigger>
+									abarbieu@calpoly.edu
+								</Navbar.Text>
+							</Container>
+						</NavDropdown>
+						<Nav.Link className="shr-btn nav-link m-auto shr-btn" eventKey="contact" href="#/contact/">
+							<img alt="Contact" src="/icons/contact.svg" width="32" height="32" />
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
 }
 
 export default MainNavbar;
