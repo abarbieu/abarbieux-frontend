82c708c5986c77409c0fc1485c355c18a0c85270
Aidan Barbieux <abarbieu@calpoly.edu>
2020-08-18 01:23:15 -0700

    scripts

--- push-prod.sh
+++ push-prod.sh
@@ -15,5 +15,5 @@ else
   git add ./front-end
   git commit -m "$1"
   git push origin master
-  ssh -t aidan@abarbieux.com "cd ~/web/prod-master/ ; rm -rf ./build ; git fetch --all ; git reset --hard origin/master ; pm2 restart frontend ; exit ; bash"
+  ./update-prod.sh
 fi
