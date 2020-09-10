5f7b10fc46136fac41bfbb1c385052e6fbeb85b0
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2020-09-10 14:48:16 -0700

    attachment support in emailer

--- src/components/contact/ContactMe.jsx
+++ src/components/contact/ContactMe.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 
-export default function ContactMe(props) {
-  const [email, setEmail] = useState('');
-  const [content, setContent] = useState('');
-  const [name, setName] = useState('');
-  const [subject, setSubject] = useState('');
-  const [sent, setSent] = useState(false);
+export default function ContactMe (props) {
+  const [ email, setEmail ] = useState('');
+  const [ content, setContent ] = useState('');
+  const [ name, setName ] = useState('');
+  const [ subject, setSubject ] = useState('');
+  const [ validated, setValidated ] = useState(false);
+  const [ attachments, setAttachments ] = useState({});
 
   let apiUrl = 'https://barbieux.dev/api/mail';
   if (process.env.NODE_ENV === 'development') {
@@ -31,67 +33,109 @@ export default function ContactMe(props) {
     event.preventDefault();
     setName(event.target.value);
   };
+  const handleFiles = (event) => {
+    event.preventDefault();
+    console.log(event.target.files.length);
+    let files = [];
+    for (let i = 0; i < event.target.files.length; i++) {
+      const file = event.target.files[i];
+      let f = {};
+      f.filename = file.name;
+      f.contentType = file.type;
+      const reader = new FileReader();
+      reader.addEventListener('load', (e) => {
+        f.content = e.target.result;
+        console.log(f);
+      });
+      reader.readAsDataURL(file);
+    }
+    setAttachments(files);
+  };
   const handleSend = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
     event.preventDefault();
     axios
-      .post(apiUrl, { replyto: email, name, subject, content })
+      .post(apiUrl, { replyto: email, name, subject, content, attachments })
       .then((res) => {
         console.log(res);
-        setSent(true);
         props.toggleContact.bind(false);
       });
   };
 
   return (
     <Modal
-      show={props.show && !sent}
+      show={props.show}
       onHide={props.toggleContact.bind(false)}
-      className="contact-modal"
+      className='contact-modal'
       centered
     >
-      <Modal.Header className="pic-modal-header" closeButton>
-        <Modal.Title>Send Me an Email</Modal.Title>
+      <Modal.Header className='pic-modal-header' closeButton>
+        <Modal.Title className='white-color'>Send Me an Email</Modal.Title>
       </Modal.Header>
-      <div className="p-2">
-        <Form onSubmit={handleSend}>
+      <div className='p-2'>
+        <Form noValidate validated={validated} onSubmit={handleSend}>
+          <Form.Row>
+            <Form.Group as={Col} sm='6' controlId='validation01'>
+              <Form.Control
+                className='align-left form-text form-obj'
+                required
+                type='email'
+                value={email}
+                onChange={handleChangeEmail}
+                placeholder='Your email address'
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              <Form.Control.Feedback type='invalid'>
+                Please provide a response email
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} sm='6'>
+              <Form.Control
+                required
+                className='align-left form-text form-obj'
+                type='text'
+                onChange={handleChangeName}
+                placeholder='Your Name'
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              <Form.Control.Feedback type='invalid'>
+                Please provide your name
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} sm='6'>
+              <Form.Control
+                className='align-left form-text form-obj'
+                type='text'
+                onChange={handleChangeSubject}
+                placeholder='Subject'
+              />
+            </Form.Group>
+            <Form.Group as={Col} sm='6'>
+              <Form.File
+                id='exampleFormControlFile1'
+                multiple
+                onChange={handleFiles}
+                className='form-obj form-file'
+              />
+            </Form.Group>
+          </Form.Row>
           <Form.Group>
             <Form.Control
-              className="align-left"
-              type="email"
-              value={email}
-              onChange={handleChangeEmail}
-              placeholder="Your email address"
-            />
-            <Form.Control
-              className="align-left"
-              type="text"
-              onChange={handleChangeName}
-              placeholder="Your Name"
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Control
-              className="align-left"
-              type="text"
-              onChange={handleChangeSubject}
-              placeholder="Subject"
-            />
-            <Form.Control
-              className="align-left"
-              type="text"
+              className='align-left form-text form-obj'
+              type='text'
               onChange={handleChangeContent}
-              as="textarea"
-              placeholder="Message"
+              as='textarea'
+              placeholder='Message'
             />
           </Form.Group>
-          <Button
-            type="submit"
-            variant="success"
-            onClick={() => {
-              setSent(true);
-              props.toggleContact(false);
-            }}
-          >
+          <Button type='submit' variant='dark'>
             Send
           </Button>
         </Form>
